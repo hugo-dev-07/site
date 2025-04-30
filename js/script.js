e2690b675c74805a1d74c9fdaecdbb089e70de93
Hugo Leite <leitehugo149@gmail.com>
2025-04-29 21:21:02 -0300

    fim

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement;
 const accordionHeader = document.querySelectorAll(".accordion-header");
+const menuLinks = document.querySelectorAll(".menu-link")
+
 
 function changeTheme () {
 
@@ -16,13 +18,20 @@ function changeTheme () {
 toggleTheme.addEventListener("click", changeTheme);
 
 //function accordion
-accordionHeader.forEach(header =>{
+accordionHeader.forEach(header => {
+
     header.addEventListener("click", () => {
-        
+
         const accordionItem = header.parentElement;
         const accordionActive = accordionItem.classList.contains("active");
 
-        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active")
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
 
-    } )
+menuLinks.forEach(item => { 
+    item.addEventListener("click", () =>{ 
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
 })
